22f8a3dd7128db233dbcc497498a2b3a64da9e5f
Matheus Ferro <matheusferroraimundo@gmail.com>
2020-10-14 17:56:02 -0300

    first commit

---
+++ src/main/java/br/com/matheusferro/todoAPP/TodoAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheusferro.todoAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheusferro/todoAPP/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusferro.todoAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
